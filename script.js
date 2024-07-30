e998c28ecbf6ee0a76589f3b3f6e117df023f462
SHORYA RAJPUT <166018833+shoryarajput21@users.noreply.github.com>
2024-07-30 13:01:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+gsap.from("#logo",{
+  opacity:0,
+  duration:0.5,
+  y:40,
+  delay:0.20
+
+})
+
+gsap.from("#btn",{
+  opacity:0,
+  duration:0.5,
+  y:40,
+  delay:0.40,
+  stagger:1
+})
+
+gsap.from("#order",{
+  opacity:0,
+  duration:1.30,
+  y:40,
+  delay:3.90,
+
+})
+
+gsap.from("#foodtext h1",{
+  opacity:0,
+  duration:2.8,
+  x:-50,
+  delay:2.7,
+  
+})
+
+gsap.from("#foodtext p",{
+  opacity:0,
+  duration:2.8,
+  x:50,
+  delay:2.7,
+  
+})
+gsap.from("#orderr",{
+  opacity:0,
+  duration:1,
+  delay:2.8,
+  x:50,
+  
+  
+})
